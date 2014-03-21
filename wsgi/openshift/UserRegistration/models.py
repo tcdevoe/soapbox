8fbbde38b03399398592a0d0ce867c5487118a08
tcdevoe <tcdevoejr@gmail.com>
2014-03-21 14:20:28 -0400

    Fixed UserRegistration models order

--- wsgi/openshift/UserRegistration/models.py
+++ wsgi/openshift/UserRegistration/models.py
@@ -6,14 +6,16 @@ from django.db import models
 
 # Not sure if we should do field validation on the
 # front end or the back end
+
+class SBLocation(models.Model) :
+	country = models.CharField(max_length=100)	# TODO : Change this to list of all valid countries
+	state = models.CharField(max_length=100)
+	city = models.CharField(max_length=100)
+
+
 class SBUser(models.Model) :
 	username = models.CharField(max_length=18)
 	password = models.CharField(max_length=18) # TODO : Need to change to make sure plaintext password is never stored
 	email = models.EmailField() # Max length of an email address
 	location = models.ForeignKey(SBLocation) # Not sure if this is how we want to store this
 
-
-class SBLocation(models.Model) :
-	country = models.CharField(max_length=100)	# TODO : Change this to list of all valid countries
-	state = models.CharField(max_length=100)
-	city = models.CharField(max_length=100)
