4b501e8e94144f3641eeface46c9bb2612cf5516
tcdevoe <tcdevoejr@gmail.com>
2014-03-21 22:30:21 -0400

    Modified User Admin Page

--- wsgi/openshift/UserRegistration/admin.py
+++ wsgi/openshift/UserRegistration/admin.py
@@ -1,4 +1,7 @@
 from UserRegistration.models import SBUser
 from django.contrib import admin
 
+class SBUserAdmin(admin.ModelAdmin) :
+	fields = ['email', 'username', 'password']
+
 admin.site.register(SBUser)
