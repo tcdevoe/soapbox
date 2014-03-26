196d697e3b13c7c3afa2e7952add291e4ccfcaf5
tcdevoe <tcdevoejr@gmail.com>
2014-03-25 20:49:29 -0400

    Learning views

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'openshift.views.home', name='home'),
     # url(r'^openshift/', include('openshift.foo.urls')),
-
+    url(r'^Register/$', 'UserRegistration.views.index'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
